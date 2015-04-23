c5b8de208b535a8930096a30bc25d581c9a78caa
Sam Zhang <sam.zhang@giantoak.com>
2015-04-23 10:34:34 -0400

    Added styling to HTML to make graph appear.

--- wiki.py
+++ wiki.py
@@ -24,14 +24,14 @@ def run(query):
 		}
 		tags=map(lambda x: x['_id'],r.json()['matches'])
 		if 'fraud' in tags:
-			g.add_node(hit['_id'],type="main",category="fraud")
+			g.add_node(hit['_id'],type="entity",category="entity")
 		else:
-			g.add_node(hit['_id'],type="main",category="document")
+			g.add_node(hit['_id'],type="entity",category="entity")
 		r2=requests.post('http://ec2-54-167-240-67.compute-1.amazonaws.com:9200/tmp/mitie/_search',data=json.dumps(payload))
 		data2=r2.json()
 		for hit2 in data2['hits']['hits']:
 			for entity in hit2['_source']['ents']:
-				g.add_node(entity['entity'],category=entity['tag'],type="main")
+				g.add_node(entity['entity'],category=entity['tag'],type="entity")
 				g.add_edge(hit['_id'],entity['entity'])
 		#g['adj']=g.adj
 		js=json_graph.node_link_data(g)
